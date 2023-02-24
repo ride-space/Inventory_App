47a382c9cb284bab9111c760e0034f7c6bd78acf
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-02-25 01:36:47 +0900

    session id add

--- front/src/pages/_app.page.tsx
+++ front/src/pages/_app.page.tsx
@@ -1,13 +1,28 @@
 import 'src/styles/reset.scss';
 import 'src/styles/global.scss';
 
-import { MantineProvider } from '@mantine/core';
+import type {
+  ColorScheme} from '@mantine/core';
+import {
+  ColorSchemeProvider,
+  MantineProvider,
+} from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
 import type { CustomAppPage } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
+import { useState } from 'react';
 import { Provider } from 'react-redux';
+import { AuthGuard } from 'src/component/AuthGuard';
 import { store } from 'src/reducks/store/store';
 
-const App: CustomAppPage = ({ Component, pageProps }) => {
+const App: CustomAppPage = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  const [colorScheme, setColorScheme] = useState<ColorScheme>('dark');
+  const toggleColorScheme = (value?: ColorScheme) => {
+    return setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+  };
   const getLayout =
     Component.getLayout ||
     ((page) => {
@@ -15,13 +30,33 @@ const App: CustomAppPage = ({ Component, pageProps }) => {
     });
 
   return (
-    <Provider store={store}>
-      <MantineProvider>
-        <NotificationsProvider>
-          {getLayout(<Component {...pageProps} />)}
-        </NotificationsProvider>
-      </MantineProvider>
-    </Provider>
+    <SessionProvider
+      session={session}
+      refetchInterval={5 * 60}
+      refetchOnWindowFocus
+    >
+      <Provider store={store}>
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider
+            theme={{
+              colorScheme: colorScheme,
+              fontFamily: 'Montserrat,sans-self',
+            }}
+          >
+            <NotificationsProvider>
+              {Component.requireAuth ? (
+                <AuthGuard>{getLayout(<Component {...pageProps} />)}</AuthGuard>
+              ) : (
+                getLayout(<Component {...pageProps} />)
+              )}
+            </NotificationsProvider>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </Provider>
+    </SessionProvider>
   );
 };
 
